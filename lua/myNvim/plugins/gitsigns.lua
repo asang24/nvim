4f1f611f033d0fade78c3c8e8aa43646bc0f5f73
asang24 <asang24@foxmail.com>
2022-11-26 10:52:00 +0800

    update gitsigns config

--- lua/myNvim/plugins/gitsigns.lua
+++ lua/myNvim/plugins/gitsigns.lua
@@ -57,9 +57,5 @@ gitsigns.setup({
 		map("n", "<leader>j", ":Gitsigns next_hunk<CR>")
 		map("n", "<leader>k", ":Gitsigns prev_hunk<CR>")
 		map("n", "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>")
-		map("n", "<leader>hd", "<cmd>Gitsigns diffthis<CR>")
-		map("n", "<leader>hs", ":Gitsigns stage_hunk<CR>")
-		map("n", "<leader>hr", ":Gitsigns reset_hunk<CR>")
-		map("n", "<leader>hu", "<cmd>Gitsigns undo_stage_hunk<CR>")
 	end,
 })
