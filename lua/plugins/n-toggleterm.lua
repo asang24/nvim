cbd2ab854e9c8b67fc69e582d92219adc62a4109
asang24 <asang24@foxmail.com>
2023-12-29 15:26:36 +0800

    update

--- lua/plugins/n-toggleterm.lua
+++ lua/plugins/n-toggleterm.lua
@@ -3,7 +3,7 @@ return {
   version = '*',
   event = 'VeryLazy',
   config = function()
-    require('toggleterm').setup {
+    require('toggleterm').setup({
       open_mapping = [[<c-/>]],
       shading_factor = '1',
       direction = 'float',
@@ -11,9 +11,9 @@ return {
       float_opts = {
         border = 'single',
       },
-    }
+    })
     local Terminal = require('toggleterm.terminal').Terminal
-    local lazygit = Terminal:new {
+    local lazygit = Terminal:new({
       cmd = 'lazygit',
       dir = 'git_dir',
       direction = 'float',
@@ -22,28 +22,17 @@ return {
       },
       -- function to run on opening the terminal
       on_open = function(term)
-        vim.cmd 'startinsert!'
-        vim.api.nvim_buf_set_keymap(
-          term.bufnr,
-          'n',
-          'q',
-          '<cmd>close<CR>',
-          { noremap = true, silent = true }
-        )
+        vim.cmd('startinsert!')
+        vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
       end,
       -- function to run on closing the terminal
       on_close = function(term)
-        vim.cmd 'startinsert!'
+        vim.cmd('startinsert!')
       end,
-    }
+    })
     function _lazygit_toggle()
       lazygit:toggle()
     end
-    vim.api.nvim_set_keymap(
-      'n',
-      '<leader>lg',
-      '<cmd>lua _lazygit_toggle()<CR>',
-      { noremap = true, silent = true }
-    )
+    vim.api.nvim_set_keymap('n', '<leader>lg', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
   end,
 }
